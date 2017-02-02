738177dc69b56903dc6ca27eb987280b97b3df23
alucardlockon <alucardlockon3@gmail.com>
2017-02-02 19:33:16 +0800

    2017-02-02 19:33:12

---
+++ ywh-net-framework/ywh-net-framework/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ywh_net_framework.Models
+{
+    public class User
+    {
+        public string Id{get; set; }
+        public string UserName{get;  set;}
+        public string Password{get;  set;}
+        public string Email{get;  set;}
+        public string HeadIcon{get;  set;}
+        public string BirthDay{get;  set;}
+        public string Age{get;  set;}
+        public string RegisterDate{get;  set;}
+        public string RegisterLoginIp{get;  set;}
+        public string LastLoginDate{get;  set;}
+        public string LastLoginIp { get; set; }
+
+    }
+}
---
+++ ywh-net-framework/ywh-net-framework/Sqls/init.sql
@@ -0,0 +1,20 @@
+﻿# 建表
+CREATE TABLE `NewTable` (
+`id`  int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id' ,
+`userName`  varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名' ,
+`password`  varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码' ,
+`email`  varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱' ,
+`headIcon`  varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '头像' ,
+`birthDay`  date NULL DEFAULT NULL COMMENT '生日' ,
+`age`  int(11) NULL DEFAULT NULL COMMENT '年龄' ,
+`registerDate`  datetime NULL DEFAULT NULL COMMENT '注册日期' ,
+`registerLoginIp`  varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '注册ip' ,
+`lastLoginDate`  datetime NULL DEFAULT NULL COMMENT '最后登陆日期' ,
+`lastLoginIp`  varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '最后登录ip' ,
+PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+AUTO_INCREMENT=1
+ROW_FORMAT=COMPACT
+;
